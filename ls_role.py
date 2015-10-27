78dd8f579eb06f83757ed516373f4f7dbac88dd4
Haemaru <rkglskame01@gmail.com>
2015-10-27 13:46:02 +0900

    Add role create/delete function
    * change LSRole.child_roles data type to dict

--- ls_role.py
+++ ls_role.py
@@ -58,18 +58,15 @@ class LSRole(object):
 
             roles_list.append(role)
 
-        for i in range(len(roles_list)):
-            if roles_list[i].parent_role_name == '':
-                roles_tree.child_roles.append(roles_list[i])
-                roles_list[i].parent_role = roles_tree
+        for child in roles_list:
+            if child.parent_role_name == '':
+                roles_tree.child_roles[child.role_name] = child
+                child.parent_role = roles_tree
             else:
-                for j in range(len(roles_list)):
-                    if roles_list[i].parent_role_name == \
-                            roles_list[j].role_name:
-                        roles_list[i].parent_role = roles_list[j]
-                        roles_list[j].child_roles.append(roles_list[i])
-
-        print(roles_tree.child_roles)
+                for parent in roles_list:
+                    if child.parent_role_name == parent.role_name:
+                        child.parent_role = parent
+                        parent.child_roles[child.role_name] = child
 
         return roles_tree
 
@@ -78,7 +75,7 @@ class LSRole(object):
         self.parent_role_name = parent_role_name
         self.attr_count = attr_count
 
-        self.child_roles = list()
+        self.child_roles = dict()
         self.parent_role = None
 
         self.file_roles = list()
@@ -108,37 +105,37 @@ class LSRole(object):
         f.write(bytearray(b'\xff' * (600 - size)))
 
 
-class LSFileRole(object):
+class LSFileRole(list):
 
     @staticmethod
     def read_by_bin(bin_data):
         return LSFileRole(bin_num(bin_data[1:9], 8), bin_data[9])
 
     def __init__(self, i_ino=0, u_acc=0):
-        self.i_ino = i_ino
-        self.u_acc = u_acc
+        self.append(('i_ino', i_ino))
+        self.append(('u_acc', u_acc))
 
     def write(self, f):
         f.write(b'\x01')
-        f.write(num_bin(self.i_ino, 8))
-        f.write(num_bin(self.u_acc, 1))
+        f.write(num_bin(self[0][1], 8))
+        f.write(num_bin(self[1][1], 1))
 
 
-class LSNetworkRole(object):
+class LSNetworkRole(list):
 
     @staticmethod
     def read_by_bin(bin_data):
         return LSNetworkRole(bin_num(bin_data[1:3], 2), bin_data[3])
 
     def __init__(self, port=0, is_allow_open=True):
-        self.port = port
-        self.is_allow_open = is_allow_open
+        self.append(('port', port))
+        self.append(('is_allow_open', is_allow_open))
 
     def write(self, f):
         f.write(b'\x02')
-        f.write(num_bin(self.port, 2))
+        f.write(num_bin(self[0][1], 2))
 
-        if self.is_allow_open:
+        if self[1][1]:
             f.write(num_bin(0, 1))
         else:
             f.write(num_bin(1, 1))
@@ -146,7 +143,7 @@ class LSNetworkRole(object):
         f.write(b'\xff' * 6)
 
 
-class LSProcessRole(object):
+class LSProcessRole(list):
 
     TYPE_PID = 0
     TYPE_INODE = 1
@@ -166,28 +163,28 @@ class LSProcessRole(object):
         is_allow_kill=True,
         is_allow_trace=True
     ):
-        self.id_value = id_value
-        self.id_type = id_type
-        self.is_allow_kill = is_allow_kill
-        self.is_allow_trace = is_allow_trace
+        self.append(('id_value', id_value))
+        self.append(('id_type', id_type))
+        self.append(('is_allow_kill', is_allow_kill))
+        self.append(('is_allow_trace', is_allow_trace))
 
     def write(self, f):
         f.write(b'\x03')
 
         flag = 0
 
-        if self.id_type == LSProcessRole.TYPE_INODE:
+        if self[1][1] == LSProcessRole.TYPE_INODE:
             flag |= 0b100
-        if self.is_allow_trace is False:
+        if self[2][1] is False:
             flag |= 0b010
-        if self.is_allow_kill is False:
+        if self[3][1] is False:
             flag |= 0b001
 
-        f.write(num_bin(self.id_value, 8))
+        f.write(num_bin(self[0][1], 8))
         f.write(num_bin(flag, 1))
 
 
-class LSBindProcess(object):
+class LSBindProcess(list):
 
     TYPE_PID = 0
     TYPE_INODE = 1
@@ -199,33 +196,33 @@ class LSBindProcess(object):
             is_bit_flagged(bin_data[9], 0b001))
 
     def __init__(self, id_value=0, id_type=TYPE_PID):
-        self.id_value = id_value
-        self.id_type = id_type
+        self.append(('id_value', id_value))
+        self.append(('id_type', id_type))
 
     def write(self, f):
         f.write(b'\xfe')
 
         flag = 0
 
-        if self.id_type == LSBindProcess.TYPE_INODE:
+        if self[1][1] == LSBindProcess.TYPE_INODE:
             flag |= 0b001
 
-        f.write(num_bin(self.id_value, 8))
+        f.write(num_bin(self[0][1], 8))
         f.write(num_bin(flag, 1))
 
 
-class LSBindUser(object):
+class LSBindUser(list):
 
     @staticmethod
     def read_by_bin(bin_data):
         return LSBindUser(bin_num(bin_data[1:5], 4))
 
     def __init__(self, uid=0):
-        self.uid = uid
+        self.append(('uid', uid))
 
     def write(self, f):
         f.write(b'\xff')
-        f.write(num_bin(self.uid, 4))
+        f.write(num_bin(self[0][1], 4))
         f.write(b'\xff' * 5)
 
 
@@ -269,19 +266,6 @@ def read_data():
 
     with open("./data.sos", "rb") as f:
         return LSRole.read_by_bin(bytearray(f.read()))
-
-
-def tree_traversal(roles_tree_node, depth):
-
-    text = ""
-    for i in range(depth):
-        text += "  "
-    text += "-"
-    text += str(roles_tree_node.role_name)
-    print(text)
-
-    for i in range(len(roles_tree_node.child_roles)):
-        tree_traversal(roles_tree_node.child_roles[i], depth + 1)
 
 
 def main():
--- ls_tui.py
+++ ls_tui.py
@@ -2,60 +2,193 @@ from ls_role import read_data, write_data, LSRole
 
 import urwid
 
-"""
-columns._set_widget_list([])
-topnode.get_child_keys(True)
-"""
+
+class LSRolesTreeWidgetPadding(urwid.Padding):
+
+    def __init__(
+        self,
+        w,
+        align=urwid.LEFT,
+        width=urwid.RELATIVE_100,
+        min_width=None,
+        left=0,
+        right=0
+    ):
+        urwid.Padding.__init__(
+            self,
+            w,
+            align,
+            width,
+            min_width,
+            left,
+            right)
+        self._focus = w[0]
+
+    @property
+    def focus(self):
+        return self._focus
+
+
+class LSRolesTreeWidgetColumns(urwid.Columns):
+
+    def keypress(self, size, key):
+        return self.contents[1][0].keypress(size, key)
+
+
+class LSRolesTreeWidgetEdit(urwid.Edit):
+
+    def __init__(
+        self,
+        caption=u"",
+        edit_text=u"",
+        multiline=False,
+        align=urwid.LEFT,
+        wrap=urwid.SPACE,
+        allow_tab=False,
+        edit_pos=None,
+        layout=None,
+        mask=None
+    ):
+        urwid.Edit.__init__(
+            self,
+            caption,
+            edit_text,
+            multiline,
+            align,
+            wrap,
+            allow_tab,
+            edit_pos,
+            layout,
+            mask)
+        self.modifiable = False
+
+    def selectable(self):
+        return self.modifiable
+
+    def set_modifiable(self, modifiable):
+        self.modifiable = modifiable
+
+    def keypress(self, size, key):
+        key = urwid.Edit.keypress(self, size, key)
 
 
 class LSRolesTreeWidget(urwid.TreeWidget):
 
+    def __init__(self, node):
+        urwid.TreeWidget.__init__(self, node)
+        self.inner_columns = self._wrapped_widget.original_widget
+        self._focus = self._wrapped_widget
+
+        if len(self._node.get_value().child_roles) == 0:
+            self.is_leaf = True
+
+        self.is_modifying = False
+
+    @property
+    def focus(self):
+        return self._focus
+
+    def get_indented_widget(self):
+        widget = self.get_inner_widget()
+        if not self.is_leaf:
+            widget = LSRolesTreeWidgetColumns([(
+                'fixed',
+                1,
+                [self.unexpanded_icon, self.expanded_icon][self.expanded]),
+                widget],
+                dividechars=1)
+
+        indent_cols = self.get_indent_cols()
+        return LSRolesTreeWidgetPadding(
+            widget,
+            width=('relative', 100),
+            left=indent_cols)
+
     def get_display_text(self):
         return self.get_node().get_value().role_name
+
+    def load_inner_widget(self):
+        self.inner_edit = LSRolesTreeWidgetEdit('', self.get_display_text())
+        return self.inner_edit
 
     def selectable(self):
         return True
 
     def keypress(self, size, key):
-        if key is not 'right':
-            key = urwid.TreeWidget.keypress(self, size, key)
+        if self.is_modifying is True:
+            if key == 'enter':
+                self.set_focus_to_icon()
+            else:
+                self.inner_edit.keypress(size, key)
+        elif key == 'enter':
+            self.set_focus_to_edit()
+        elif key == 'tab':
+            if self.expanded is True:
+                self.expanded = False
+                self.update_expanded_icon()
+            else:
+                self.expanded = True
+                self.update_expanded_icon()
+        else:
+            return key
 
-        return key
+    def set_focus_to_edit(self):
+        self.is_modifying = True
+        self.inner_edit.set_modifiable(True)
+        self.inner_columns.set_focus_column(1)
+        # self.inner_edit.move_cursor_to_coords((20, ), 15, 0)
+
+    def set_focus_to_icon(self):
+        self.is_modifying = False
+        self.inner_edit.set_modifiable(False)
+        self.inner_columns.set_focus_column(0)
 
 
-class LSRolesTreeNode(urwid.TreeNode):
+class LSRolesTreeNode(urwid.ParentNode):
 
-    def load_widget(self):
-        return LSRolesTreeWidget(self)
-
-
-class LSRolesParentNode(urwid.ParentNode):
+    def get_parent(self):
+        return urwid.ParentNode.get_parent(self)
 
     def load_parent(self):
-        return self.get_value().parent_role
+        log('=================load parent call===================')
 
     def load_widget(self):
         return LSRolesTreeWidget(self)
 
     def load_child_keys(self):
-        return range(len(self.get_value().child_roles))
+        return self.get_value().child_roles.keys()
+
+    def get_child_node(self, key, reload=True):
+        if key not in self._children or reload is True:
+            self._children[key] = self.load_child_node(key)
+        return self._children[key]
+
+    def get_child_index(self, key):
+        try:
+            return self.get_child_keys(True).index(key)
+        except ValueError:
+            return None
 
     def load_child_node(self, key):
-        childdata = self.get_value().child_roles[key]
-        childdepth = self.get_depth() + 1
-        if len(childdata.child_roles) == 0:
-            childclass = LSRolesTreeNode
+        if key is None:
+            log('error!')
+            return None
         else:
-            childclass = LSRolesParentNode
-        return childclass(childdata, parent=self, key=key, depth=childdepth)
+            return LSRolesTreeNode(
+                value=self.get_value().child_roles[key],
+                parent=self,
+                key=key,
+                depth=self.get_depth() + 1
+            )
 
 
 class LSRolesTreeListBox(urwid.TreeListBox):
 
-    def __init__(self, body, attr_types):
+    def __init__(self, body, attr_types_list):
         urwid.TreeListBox.__init__(self, body)
-        self.attr_types = attr_types
-        self.update_attr_types()
+        self.create_count = 1
+        self.attr_types_list = attr_types_list
+        self.set_role()
 
     def change_focus(
         self,
@@ -74,14 +207,61 @@ class LSRolesTreeListBox(urwid.TreeListBox):
             coming_from,
             cursor_coords,
             snap_rows)
-        self.update_attr_types()
+        self.set_role()
 
     def keypress(self, size, key):
-        return urwid.TreeListBox.keypress(self, size, key)
+        key = self.__super.keypress(size, key)
 
-    def update_attr_types(self):
-        self.attr_types.set_role(self.get_focus()[1].get_value())
-        self.attr_types.update_attrs()
+        if key == 'c':
+            self.create_role(size)
+        elif key == 'd':
+            self.delete_role(size)
+        else:
+            return key
+
+    def unhandled_input(self, size, input):
+        return input
+
+    def create_role(self, size):
+        focused_node = self.get_focus()[1]
+        focused_role = focused_node.get_value()
+
+        new_role_name = 'new_role' + str(self.create_count)
+        self.create_count += 1
+        new_role = LSRole(new_role_name)
+        new_node = LSRolesTreeNode(
+            value=new_role,
+            parent=focused_node,
+            key=new_role_name,
+            depth=focused_node.get_depth() + 1
+        )
+
+        focused_role.child_roles[new_role_name] = new_role
+        focused_node._children[new_role_name] = new_node
+        focused_node.get_child_keys(True)
+
+        self.change_focus(size, new_node)
+
+    def delete_role(self, size):
+        focused_node = self.get_focus()[1]
+        focused_role = focused_node.get_value()
+        parent_node = focused_node.get_parent()
+
+        if parent_node is not None:
+            parent_role = parent_node.get_value()
+
+            del parent_role.child_roles[focused_role.role_name]
+            del parent_node._children[focused_role.role_name]
+
+            for child in focused_node._children.items():
+                parent_role.child_roles[child[0]] = child[1].get_value()
+                parent_node._children[child[0]] = child[1]
+            parent_node.get_child_keys(True)
+
+            self.change_focus(size, parent_node)
+
+    def set_role(self):
+        self.attr_types_list.set_role(self.get_focus()[1].get_value())
 
 
 class LSRolesTreeWalker(urwid.TreeWalker):
@@ -92,9 +272,9 @@ class LSRolesTreeWalker(urwid.TreeWalker):
 
 class LSAttrTypesListBox(urwid.ListBox):
 
-    def __init__(self, body, attrs):
+    def __init__(self, body, attrs_list):
         urwid.ListBox.__init__(self, body)
-        self.attrs = attrs
+        self.attrs_list = attrs_list
 
     def change_focus(
         self,
@@ -113,25 +293,24 @@ class LSAttrTypesListBox(urwid.ListBox):
             coming_from,
             cursor_coords,
             snap_rows)
-        self.update_attrs()
+        self.set_attr_type()
 
     def set_role(self, role):
         self.role = role
+        self.set_attr_type()
 
-    def update_attrs(self):
+    def set_attr_type(self):
         focus = self.get_focus()[1]
-        role_types = [
+        attr_types = [
             self.role.file_roles,
             self.role.network_roles,
             self.role.process_roles,
             self.role.bind_processes,
             self.role.bind_users
         ]
-        role = role_types[focus]
+        attr_type = attr_types[focus]
 
-        del self.attrs.body[:]
-        for i in range(len(role)):
-            self.attrs.body.append(urwid.Button("role " + str(i)))
+        self.attrs_list.set_attr_type(attr_type)
 
 
 class LSAttrTypesListWalker(urwid.SimpleFocusListWalker):
@@ -142,6 +321,10 @@ class LSAttrTypesListWalker(urwid.SimpleFocusListWalker):
 
 class LSAttrsListBox(urwid.ListBox):
 
+    def __init__(self, body, values_list):
+        urwid.ListBox.__init__(self, body)
+        self.values_list = values_list
+
     def change_focus(
         self,
         size,
@@ -159,10 +342,60 @@ class LSAttrsListBox(urwid.ListBox):
             coming_from,
             cursor_coords,
             snap_rows)
-        log("LSAttrListBox.change_focus")
+        self.set_attr()
+
+    def keypress(self, size, key):
+        if key == 'c':
+            log(self.attr_type.__class__)
+
+        return urwid.ListBox.keypress(self, size, key)
+
+    def set_attr_type(self, attr_type=None):
+        if attr_type is not None:
+            self.attr_type = attr_type
+
+        del self.body[:]
+
+        for attr in attr_type:
+            self.body.append(urwid.Edit(attr[0][0] + ' : ' + str(attr[0][1])))
+
+        self.set_attr()
+
+    def set_attr(self):
+        focus = self.get_focus()[1]
+
+        if focus is not None:
+            attr = self.attr_type[focus]
+        else:
+            attr = None
+
+        self.values_list.set_attr(attr)
 
 
 class LSAttrsListWalker(urwid.SimpleFocusListWalker):
+
+    def __init__(self, contents):
+        urwid.SimpleFocusListWalker.__init__(self, contents)
+
+
+class LSAttrValuesListBox(urwid.ListBox):
+
+    def __init__(self, body):
+        urwid.ListBox.__init__(self, body)
+
+    def set_attr(self, attr):
+        self.attr = attr
+
+        del self.body[:]
+
+        if attr is not None:
+            for value in attr:
+                self.body.append(urwid.Edit(
+                    str(value[0]) + ' : ',
+                    str(value[1])))
+
+
+class LSAttrValuesListWalker(urwid.SimpleFocusListWalker):
 
     def __init__(self, contents):
         urwid.SimpleFocusListWalker.__init__(self, contents)
@@ -178,6 +411,7 @@ class LSColumns(urwid.Columns):
 
 def exit(key):
     if key in ('q', 'Q'):
+        log('-----------------exit------------------')
         raise urwid.ExitMainLoop()
 
 
@@ -188,11 +422,15 @@ def log(text):
     f.write(str(text) + '\n')
     f.flush()
 
+
 write_data()
 roles_data = read_data()
 
+attr_values_list_walker = LSAttrValuesListWalker([])
+attr_values_list_box = LSAttrValuesListBox(attr_values_list_walker)
+
 attrs_list_walker = LSAttrsListWalker([])
-attrs_list_box = LSAttrsListBox(attrs_list_walker)
+attrs_list_box = LSAttrsListBox(attrs_list_walker, attr_values_list_box)
 
 attr_types_list_walker = LSAttrTypesListWalker([
     urwid.Button("File Roles"),
@@ -204,23 +442,62 @@ attr_types_list_box = LSAttrTypesListBox(
     attr_types_list_walker,
     attrs_list_box)
 
-roles_root_node = LSRolesParentNode(roles_data)
+roles_root_node = LSRolesTreeNode(roles_data)
+log('origin ' + str(id(roles_root_node)))
+log(type(roles_root_node))
+
 roles_tree_walker = LSRolesTreeWalker(roles_root_node)
 roles_tree_box = LSRolesTreeListBox(
     roles_tree_walker,
     attr_types_list_box)
 
-columns_data = [roles_tree_box, attr_types_list_box, attrs_list_box]
+columns_data = [
+    roles_tree_box,
+    attr_types_list_box,
+    attrs_list_box,
+    attr_values_list_box]
 columns = LSColumns(columns_data)
 
-header = urwid.Text("this is header")
+root_widget = roles_root_node.get_widget()
+
+
+header = urwid.Edit("test")
 footer = urwid.Text("this is footer")
 top_most = urwid.Frame(columns, header=header, footer=footer)
 
-"""
-top_most = urwid.ListBox(urwid.SimpleFocusListWalker([]))
-top_most.body.append(urwid.Button("test"))
-"""
+
+def node_traversal(node, depth):
+    text = ""
+    for i in range(depth):
+        text += "  "
+    text += "-"
+    text += str(node.get_value().role_name + ' - ' + \
+        str([node._children[n].get_value().role_name for n in node._children]))
+    log(text)
+
+    for key in node.get_value().child_roles.keys():
+        node_traversal(node._children[key], depth + 1)
+
+
+def tree_update(node):
+    log(node.get_value().role_name)
+    for key in node.get_child_keys(True):
+        node._children.clear()
+        node.get_child_node(key, True)
+        tree_update(node._children[key])
+
+
+def tree_traversal(roles_tree_node, depth):
+
+    text = ""
+    for i in range(depth):
+        text += "  "
+    text += "-"
+    text += str(roles_tree_node.role_name)
+    log(text)
+
+    for key in roles_tree_node.child_roles.keys():
+        tree_traversal(roles_tree_node.child_roles[key], depth + 1)
 
 
 def main():
