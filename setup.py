b61bedebd7132ec25b016e914ba176c0fc478e5e
Haemaru <rkglskame01@gmail.com>
2015-11-10 02:07:42 +0900

    Add urwid install requirement

--- setup.py
+++ setup.py
@@ -2,5 +2,8 @@ from distutils.core import setup, Extension
 
 setup(
     name='ls_syscall',
+    install_requires=[
+        "urwid==1.3.0",
+    ],
     ext_modules=[Extension('ls_syscall', ['ls_syscall.c'])]
 )
