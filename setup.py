7c9d6ce0e7d36fc79c301d712145c4fb1f71d4e1
Luavis <luaviskang@gmail.com>
2015-11-11 18:33:07 +0900

    Delete ls_tui and modify setup.py

--- setup.py
+++ setup.py
@@ -1,21 +1,34 @@
-from distutils.core import setup, Extension
+PYPY = hasattr(sys, 'pypy_version_info')
 
-setup(
-    name='ls_syscall',
-    version='0.1.0',
-    license='MIT',
-    description='SOS-Manager, Role based mandatory access control setup',
-    long_description='',
-    author='Haemaru',
-    author_email='rkglskame01@gmail.com',
-    url='https://github.com/Haemaru/SOS-Manager',
-    classifiers=[
-        'Development Status :: 2 - Pre-Alpha',
-        'License :: Freeware',
-        'Operation System :: POSIX'],
-    scripts=['ls_tui'],
-    install_requires=[
-        "urwid==1.3.0",
-    ],
-    ext_modules=[Extension('ls_syscall', ['ls_syscall.c'])]
-)
+try:
+    from setuptools import Extension, setup
+except ImportError:
+    if PYPY:
+        # need setuptools for include_package_data to work
+        raise
+    from distutils.core import Extension, setup
+
+def install():
+    setup(
+        name='ls_syscall',
+        version='0.1.0',
+        license='MIT',
+        description='SOS-Manager, Role based mandatory access control setup',
+        long_description='',
+        author='Haemaru',
+        author_email='rkglskame01@gmail.com',
+        url='https://github.com/Haemaru/SOS-Manager',
+        classifiers=[
+            'Development Status :: 2 - Pre-Alpha',
+            'License :: Freeware',
+            'Operation System :: POSIX'],
+        scripts=['ls_tui'],
+        install_requires=[
+            "urwid==1.3.0",
+        ],
+        ext_modules=[Extension('ls_syscall', ['ls_syscall.c'])]
+    )
+
+if __name__ == '__main__':
+    install()
+
